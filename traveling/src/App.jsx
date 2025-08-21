e4f9aadcf44b2a0960af5605b6e604871639251f
Qamar5656 <qamar4157@gmail.com>
2025-08-21 17:43:12 +0500

    updating package.json file

--- traveling/src/App.jsx
+++ traveling/src/App.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DestinationDetails from "./Destinations Content/DestinationDetails";
 import MainDestinationComp from "./Destinations Content/MainDestinationComp";
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      {/* ✅ Navbar goes here */}
       <Navbar />
+
+      {/* ✅ Only Route components inside Routes */}
       <Routes>
         <Route path="/" element={<MainDestinationComp />} />
         <Route path="/destination/:id" element={<DestinationDetails />} />
