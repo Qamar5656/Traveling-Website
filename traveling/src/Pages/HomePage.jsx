25fcd669a238685025857064cf96538b9be37a6d
Qamar5656 <qamar4157@gmail.com>
2025-08-23 23:38:04 +0500

    updating home page

--- traveling/src/Pages/HomePage.jsx
+++ traveling/src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import MainDestinationComp from "../Destinations Content/MainDestinationComp";
 import Footer from "../components/Footers";
+import MainTrainsPage from "../Trains Details/MainTrainsPage";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
         <Navbar />
         <Hero />
         <MainDestinationComp />
+        <MainTrainsPage />
         <Footer />
       </div>
     </>
