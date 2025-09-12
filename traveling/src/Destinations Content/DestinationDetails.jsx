55e8929bbe1dce36b89b0f91f6fa993521086d9d
Qamar5656 <qamar4157@gmail.com>
2025-09-12 16:29:50 +0500

    updating destination navigation details page

--- traveling/src/Destinations Content/DestinationDetails.jsx
+++ traveling/src/Destinations Content/DestinationDetails.jsx
@@ -1,26 +1,36 @@
 import { useLocation } from "react-router-dom";
+import Footer from "../components/Footers";
 
 const DestinationDetails = () => {
   const location = useLocation();
   const { destination } = location.state || {};
 
   if (!destination) {
-    return <p>No destination data found. Please go back and try again.</p>;
+    return (
+      <p className="mt-14">
+        No destination data found. Please go back and try again.
+      </p>
+    );
   }
 
   return (
-    <div className="container">
-      <h1>{destination.locationName}</h1>
-      <img src={destination.img} alt={destination.locationName} />
-      <p>{destination.description}</p>
-      <ul>
-        <li>Price: {destination.price}</li>
-        <li>Rating: {destination.rating}</li>
-        <li>Duration: {destination.duration}</li>
-        <li>Transport: {destination.transport}</li>
-        <li>Best Time: {destination.bestTime}</li>
-      </ul>
-    </div>
+    <>
+      <div className="container ">
+        <h1>{destination.locationName}</h1>
+        <img src={destination.img} alt={destination.locationName} />
+        <p>{destination.description}</p>
+        <ul>
+          <li>Price: {destination.price}</li>
+          <li>Rating: {destination.rating}</li>
+          <li>Duration: {destination.duration}</li>
+          <li>Transport: {destination.transport}</li>
+          <li>Best Time: {destination.bestTime}</li>
+        </ul>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
