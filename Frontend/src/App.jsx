1b5cbe45e2add3d426d21d4ef12017240d3da0ab
Qamar5656 <qamar4157@gmail.com>
2025-09-18 20:54:48 +0500

    adding routes of the pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import DestinationDetails from "./Destinations Content/DestinationDetails";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import About from "./Pages/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
         <Route path="/destination" element={<DestinationDetails />} />
       </Routes>
     </Router>
